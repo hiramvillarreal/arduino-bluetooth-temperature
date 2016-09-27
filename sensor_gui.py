94586d30045719d3c316614a5e963c4a08993fe9
hiramvillarreal <hiramvillarreal@hotmail.com>
2016-09-27 11:16:28 -0600

    third commit

--- sensor_gui.py
+++ sensor_gui.py
@@ -1,56 +1,63 @@
-#!/usr/bin/env python
-# Graphical User Interface for the DHT sensor
+#Humidity and Temperature Reader for Honeywell Sensors
+# Graphical User Interface for the Honeywell humidity sensors HIH####
 
 # Imports
 import time
-import Adafruit_DHT
-import serial
+import subprocess
 from Tkinter import *
-sensor = Adafruit_DHT.DHT11 #DHT11/DHT22/AM2302
-pin = 4
 
+# Main Tkinter application
 class Application(Frame):
 
 	# Measure data from the sensor
 	def measure(self):
-                hum, temp = Adafruit_DHT.read_retry(sensor, pin)
-		data = (str(temp) + "," + str(hum))
+
+		# Request data and read the answer. Call the Honeywell script at home directory, the original C file was modified to output like: (32,32) hum,temp.
+		output = subprocess.Popen(['/home/pi/i2cHoneywellHumidity/i2cHoneywellHumidity'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                data =  output.communicate()[0]
+
 		# If the answer is not empty, process & display data
 		if (data != ""):
 			processed_data = data.split(",")
 
-			self.hum_data.set(str(processed_data[1]))
-			self.humidity.pack()
-			
-			self.temp_data.set("% Humedad:" )
-			self.temperature.pack()
+			self.hum_data.set(str(processed_data[0]))
+                        self.humidity.pack()
 
-		# Wait 1 second between each measurement
-		self.after(10000,self.measure)
+                        self.label_data.set("% Humedad:")
+                        self.label.pack()
+
+                        self.temp_data.set("Temperatura: " + str(processed_data[1]) + " C")
+                        self.temperature.pack()
+                
+		# Wait time in milliseconds between each measurement
+		self.after(30000,self.measure)
 
 	# Create display elements
 	def createWidgets(self):
 
-		self.humidity = Label(self, textvariable=self.hum_data, font=('Verdana', 230, 'bold'))
-		self.hum_data.set("Humidity")
-		self.humidity.pack()
+                self.label = Label(self, textvariable=self.label_data, font=('Verdana', 40, 'bold'))
+                self.label.pack()
 		
-		self.temperature = Label(self, textvariable=self.temp_data, font=('Verdana', 40, 'bold'))
-		self.temp_data.set("Temperature")
-		self.temperature.pack()
+		self.humidity = Label(self, textvariable=self.hum_data, background='red', foreground='yellow', font=('Verdana', 240, 'bold'))
+#                self.humidity = Label(self, textvariable=self.hum_data,  font=('Verdana', 240, 'bold'))
+		self.humidity.pack()                
+
+                self.temperature = Label(self, textvariable=self.temp_data, font=('Verdana', 30))
+                self.temperature.pack()
+
 
 	# Init the variables & start measurements
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
 		self.temp_data = StringVar()
 		self.hum_data = StringVar()
+                self.label_data = StringVar()
 		self.createWidgets()
 		self.pack()
 		self.measure()
 
-
 # Create and run the GUI
 root = Tk()
-root.attributes('-fullscreen', True)
+root.attributes('-fullscreen', True) #Comment this to disable fullscreen display
 app = Application(master=root)
 app.mainloop()
